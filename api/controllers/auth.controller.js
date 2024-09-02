b239919b0237a8e43c9b2dc8b5e90f8dea72d88c
rahulsing23 <rahul561singh@gmail.com>
2024-09-02 14:22:34 +0530

    create middleware and custom error handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
-export const signup = async (req, res) =>{
+
+
+export const signup = async (req, res, next) =>{
     const { username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
     const newUser = new User({
@@ -16,7 +18,7 @@ export const signup = async (req, res) =>{
     }
     catch(error)
     {
-        res.status(500).json(error.message)
+        next(error);
     }
     
 }
--- api/index.js
+++ api/index.js
@@ -23,6 +23,20 @@ app.listen(process.env.PORT, ()=>{
 
 app.use(express.json())
 
+// test api
 app.use('/api/user', userRoute);
 
+// signup api
 app.use('/api/auth', authRoute);
+
+// middleware 
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error'
+
+    return res.status(statusCode).json({
+        success: false,
+        statusCode: statusCode,
+        message: message
+    })
+})
