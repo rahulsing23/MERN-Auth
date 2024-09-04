8cbea2f6f6020c085810fe53908a03d7cb315b48
rahulsing23 <rahul561singh@gmail.com>
2024-09-04 15:15:30 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
-
-
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
 export const signup = async (req, res, next) =>{
     const { username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
@@ -21,4 +23,30 @@ export const signup = async (req, res, next) =>{
         next(error);
     }
     
+}
+
+export const signin = async (req, res, next) =>{
+    const { email, password } = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser)
+        {
+            return next(errorHandler(404, 'User Not Found!'))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword)
+        {
+            next(errorHandler(401, 'Wrong Credentials'))
+        }
+
+        const token  = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+        const {password:hashedPassword, ...rest} = validUser._doc;
+        const expiryDate = new Date(Date.now()+3600000); // 1 hour
+        res.cookie('access_token', token, {httpOnly:true , expires:expiryDate}).status(200).json(rest)
+    }
+    catch(error)
+    {
+        next(error)
+    }
+
 }
